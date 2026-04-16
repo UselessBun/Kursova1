d172c5970a7cdb69d59d1cb78ff0e118ce9c073f
Ploskonos Sophia <ploskonossonya@gmail.com>
2026-04-16 18:33:46 +0300

    Added struct

--- Tasj14.cc
+++ Tasj14.cc
@@ -1,5 +1,25 @@
 #include <iostream>;
 using namespace std;
+struct domino{
+    int a,b;
+    int x1,y1,x2,y2;
+    domino(){
+        a=-1;
+        b=-1;
+        x1=-1;
+        y1=-1;
+        x2=-1;
+        y2=-1;
+    }
+    domino(int a,int b,int x1,int y1,int x2,int y2){
+        this->a=a;
+        this->b=b;
+        this->x1=x1;
+        this->y1=y1;
+        this->x2=x2;
+        this->y2=y2;
+    }
+};
 class domino_half{
     int a;
     bool taken;
@@ -14,11 +34,9 @@ ostream &operator<<(ostream &out, const domino_half &d){
         out<<d.a;
         return out;
 }
-int find(int dtofind[2], domino_half riddle[6][10]){
-    
-}
+
 int main() {
-    domino_half riddle[6][10]={
+    const domino_half riddle[6][10]={
         {0,0,1,1,2,3,3,5,4,0},
         {3,3,2,5,6,6,3,4,2,0},
         {6,3,0,6,1,1,6,4,2,0},
@@ -26,6 +44,7 @@ int main() {
         {2,4,4,0,0,5,4,5,5,5},
         {1,1,4,6,0,5,1,3,5,3}
     };
+    domino answer[28];
     cout<<"The riddle: "<<endl;
     for(int i=0;i<6;i++){
         for(int j=0;j<10;j++){
@@ -34,33 +53,11 @@ int main() {
         cout<<endl;
     }
     cout<<"Found dominos: "<<endl;
-    cout<<"0-0, at adresses: "<<endl;
-    cout<<"0-1, at adresses: "<<endl;
-    cout<<"0-2, at adresses: "<<endl;
-    cout<<"0-3, at adresses: "<<endl;
-    cout<<"0-4, at adresses: "<<endl;
-    cout<<"0-5, at adresses: "<<endl;
-    cout<<"0-6, at adresses: "<<endl;
-    cout<<"1-1, at adresses: "<<endl;
-    cout<<"1-2, at adresses: "<<endl;
-    cout<<"1-3, at adresses: "<<endl;
-    cout<<"1-4, at adresses: "<<endl;
-    cout<<"1-5, at adresses: "<<endl;
-    cout<<"1-6, at adresses: "<<endl;
-    cout<<"2-2, at adresses: "<<endl;
-    cout<<"2-3, at adresses: "<<endl;
-    cout<<"2-4, at adresses: "<<endl;
-    cout<<"2-5, at adresses: "<<endl;
-    cout<<"2-6, at adresses: "<<endl;
-    cout<<"3-3, at adresses: "<<endl;
-    cout<<"3-4, at adresses: "<<endl;
-    cout<<"3-5, at adresses: "<<endl;
-    cout<<"3-6, at adresses: "<<endl;
-    cout<<"4-4, at adresses: "<<endl;
-    cout<<"4-5, at adresses: "<<endl;
-    cout<<"4-6, at adresses: "<<endl;
-    cout<<"5-5, at adresses: "<<endl;
-    cout<<"5-6, at adresses: "<<endl;
-    cout<<"6-6, at adresses: "<<endl;
+    for(int i=0;i<7;i++){
+        for(int j=0;j<7;j++){
+            cout<<i<<'-'<<j<<" at adress: ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
